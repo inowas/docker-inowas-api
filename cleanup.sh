396d13a44988950232aa90a87084bce8b03c7241
Ralf Junghanns <ralf.junghanns@adensis.com>
2018-01-09 15:12:32 +0100

    Update deployment-scripts

--- cleanup.sh
+++ cleanup.sh
@@ -4,9 +4,9 @@
 docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
     
 # remove unused images:
-    docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
 
 # remove unused volumes:
-find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
-  docker ps -aq | xargs docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)'
-  ) | xargs -r rm -fr
+#find '/var/lib/docker/volumes/' -mindepth 1 -maxdepth 1 -type d | grep -vFf <(
+#  docker ps -aq | xargs docker inspect | jq -r '.[] | .Mounts | .[] | .Name | select(.)'
+#  ) | xargs -r rm -fr
--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 cd ../app && git pull && cd ../docker
-docker-compose down
+cd ../api && git pull && cd ../docker
+
+docker-compose stop app
 docker-compose build --no-cache app
-docker-compose -f docker-compose.yml up -d --no-recreate
 docker-compose -f app.yml up -d --no-recreate
 docker-compose ps
+
+# remove exited containers:
+docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v
+
+# remove unused images:
+docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
