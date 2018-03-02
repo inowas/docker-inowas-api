f4007424fedbc88171d954ae1ceb908e0a28004e
Ralf Junghanns <ralf.junghanns@adensis.com>
2018-03-02 16:17:45 +0100

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -4,8 +4,8 @@ cd ../app && git pull && cd ../docker
 cd ../api && git pull && cd ../docker
 
 #docker-compose stop app
-docker-compose build --no-cache app
 docker-compose down
+docker-compose build --no-cache app
 docker-compose -f docker-compose.yml up -d --no-recreate
 docker-compose -f app.yml up -d --no-recreate
 docker-compose ps
