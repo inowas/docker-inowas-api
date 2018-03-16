2231a97b33138662502f1b6700ed5a4641d24eb0
Ralf Junghanns <ralf.junghanns@adensis.com>
2018-03-16 16:02:37 +0100

    Updates the rebuild-script

--- rebuild.sh
+++ rebuild.sh
@@ -3,7 +3,13 @@
 cd ../app && git pull && git submodule update && cd ../docker
 cd ../api && git pull && git submodule update && cd ../docker
 
-#docker-compose stop app
+# Doctrine schema update
+docker-compose exec php ./bin/console doctrine:schema:update --force
+docker-compose exec php ./bin/console cache:clear -e prod
+docker-compose exec php ./bin/console cache:clear -e dev
+docker-compose exec php ./bin/console cache:clear -e test
+
+# docker-compose stop app
 docker-compose down
 docker-compose build --no-cache app
 docker-compose -f docker-compose.yml up -d --no-recreate
